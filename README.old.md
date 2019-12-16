58e6cedfc98d5380c2935a7f9abb824d1af0d33b
Mamady Sillah <supurpisces310@msn.com>
2019-12-15 22:04:01 -0500

    created readme file

--- README.old.md
+++ README.old.md
@@ -1,1 +1,11 @@
-# ui-react-
+# Purpose
+This project uses an API call of random cat photos to display them into a slideshow. Utilize HTML, CSS, React/JSX. 
+
+# Author 
+Mamady Sillah 
+
+# Acknowledgements
+Cat photos API https://aws.random.cat/meow?ref=apilist.fun 
+
+# License
+VSCODE?
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './Button'
 class App extends Component {
@@ -7,34 +6,66 @@ class App extends Component {
   constructor() {
     super()
     this.state ={
+      count: 0,
       catArray: [],
-      cats: ""
     }
     //binders go here
-   
+    this.nextButton = this.nextButton.bind(this);
+    this.previousButton = this.previousButton.bind(this);
     this.thisButton = this.thisButton.bind(this);
   }
   //api key e2ed5ae39e5d1bf34dcdc6f75ad6b13e  
   thisButton() {
+    
     fetch("https://aws.random.cat/meow?ref=apilist.fun")
   .then(res => res.json())
   .then(res => {
     this.setState({cats: res.file})
-    console.log(res)
-    })}
- 
+    // console.log(res)
+    if(this.state.catArray.length <= 15){
+      this.setState(prevState => ({
+      catArray: [...prevState.catArray, res]
+    }))
+    
+    }else{this.state.catArray.shift();} 
+
+    })
+  }
+  nextButton (){
+    if(this.state.count < 15){
+    this.setState({count: this.state.count + 1})
+    }else {
+      console.log(this.state.count)
+    }
+  
+  }  
+  previousButton (){
+    if(this.state.count > 0){
+    this.setState({count: this.state.count - 1})
+    }else {
+      console.log(this.state.count)
+    }
+  } 
 
 render() {
+   
+  const count = this.state.count
+  const img = this.state.catArray[count] 
   return (
     <div className="App">
       <header className="App-header">
       <h1 className="App-title">Random Cats</h1>
       </header>
+       <div className="topButtons">
+         <Button thisButton={this.nextButton}label={'Next Cat'}/>
+         <Button thisButton={this.previousButton}label={"Last Cat"}/>
+        </div> 
+      
       <div className="litterbox">
-        <img src={this.state.cats} alt='cat image'></img>
+        <img src={this.state.cats} alt='cat image'/>
         </div>
         <div>
-      <Button thisButton={this.thisButton} label={'next res'}/>
+      <Button thisButton={this.thisButton}label={'Add Photo'}/>
       </div>
       
     </div>
